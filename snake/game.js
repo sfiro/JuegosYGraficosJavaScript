74d4635801c23127fd7d19476f785271839ef36d
Debbie Arredondo <chevi6@gmail.com>
2024-04-30 10:47:15 -0500

    Se agregan comentarios al codigo

--- snake/game.js
+++ snake/game.js
@@ -1,14 +1,16 @@
 var s;
-var scl = 20;
+var scl = 10;
 var food;
 
 function setup(){
+    //crea la pantalla y la serpiente
     createCanvas(600, 600);
     s = new Snake();
     pickLocation();
 }
 
 function pickLocation(){
+    //elige una ubicación aleatoria para la comida
     var cols = floor(width/scl);
     var rows = floor(height/scl);
     food = createVector(floor(random(cols)), floor(random(rows)));
@@ -16,25 +18,25 @@ function pickLocation(){
 }
 
 function draw(){
+    //dibuja la serpiente y la comida
     background(0);
-    
     s.eat();
-    
     s.death();
     s.update();
     s.show();
     frameRate(10);
-    
 
     fill(255, 0, 100);
     rect(food.x, food.y, scl, scl);
 }
 
 function mousePressed(){
+    // incrementa el tamaño de la serpiente al hacer click
     s.total = s.total + 1;
 }
 
 function keyPressed(){
+    //cambia la dirección de la serpiente al presionar una tecla
     if (keyCode === UP_ARROW){
         s.dir(0, -1);
     } else if (keyCode === DOWN_ARROW){
--- snake/snake.js
+++ snake/snake.js
@@ -1,5 +1,10 @@
+// function snake game 
 
 function Snake(){
+    // x,y define la posición de la cabeza de la serpiente
+    // xspeed, yspeed define la dirección de la serpiente
+    // total define el tamaño de la serpiente
+    // tail es un arreglo que guarda la posición de las partes de la serpiente
     this.x = 0;
     this.y = 0;
     this.xspeed = 1;
@@ -8,6 +13,8 @@ function Snake(){
     this.tail = [];
 
     this.update = function(){
+        // mueve la serpiente
+
         if (this.total === this.tail.length){
             console.log(this.total);
             for (var i = 0; i < this.tail.length - 1; i++){
@@ -21,6 +28,7 @@ function Snake(){
         this.limits();
     }
     this.death = function(){
+        // revisa si la serpiente se ha comido a si misma
         for (var i = 0; i < this.tail.length; i++){
             var pos = this.tail[i];
             var d = dist(this.x, this.y, pos.x, pos.y);
@@ -47,6 +55,7 @@ function Snake(){
     }
 
     this.show = function(){
+        // muestra la serpiente
         fill(255);
         for (var i = 0; i < this.total; i++){
             rect(this.tail[i].x, this.tail[i].y, scl, scl);
@@ -55,6 +64,8 @@ function Snake(){
     }
 
     this.dir = function(x, y){
+        // cambia la dirección de la serpiente
+        // la serpiente no puede cambiar de dirección en sentido contrario
         if (this.xspeed === -1 && x === 1){
             return;
         }
@@ -72,6 +83,7 @@ function Snake(){
     }
 
     this.eat = function(){
+        // revisa si la serpiente ha comido la comida
         if (this.x === food.x && this.y === food.y){
             pickLocation();
             this.total++;
